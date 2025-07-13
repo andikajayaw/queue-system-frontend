8a308c0c678a707d80bdb643da43e4770729f4de
andikajayaw <andikajayawiguna@gmail.com>
2025-07-13 08:32:00 +0800

    remove files

--- pages/apps.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-import Apps from "@/features/apps/index.vue";
-</script>
-<template>
-  <Apps />
-</template>
--- pages/tasks.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-import Tasks from "@/features/tasks/index.vue";
-</script>
-<template>
-  <Tasks />
-</template>
